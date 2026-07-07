b6553a570936ef7ee6e4f746f6d6993471a39b17
MuhammadAhmadF2005 <istrikepro13ahmadfaisal@gmail.com>
2026-07-07 17:32:41 +0500

    Roadma dir made follow along on this journey

---
+++ Roadmap/exception.py
@@ -0,0 +1,30 @@
+def exception_handling():
+    try:
+        number = int(input("Enter a divisor: "))
+        result = 10 / number
+    except ZeroDivisionError:
+        print("Cannot divide by zero")
+    except ValueError:
+        print("Invalid input")
+    else:
+        print("Result is ", result)
+    finally:
+        print("Program finished") 
+
+###now we define our own errorType using OOP
+
+class InsufficientFundsError(Exception):
+    pass
+
+def withdraw(amount, balance):
+    try:
+        if amount > balance:
+            raise InsufficientFundsError("insufficient funds")
+        balance = balance - amount
+        print(f"New balance: {balance}")
+    except InsufficientFundsError as e:
+        print(e)
+    else:
+        print("withdrawal successful")
+
+withdraw(100, 500)
